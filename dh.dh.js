822f1b3ffebdc50e2239b98f2d0b52a10be20dd9
Salim KAYABAŞI <salim.kayabasi@gmail.com>
2012-08-12 03:44:28 +0300

    new configs for ads

--- dh.dh.js
+++ dh.dh.js
@@ -1,9 +1,33 @@
-﻿if($ === 'undefined') {
+﻿
+if($ === 'undefined') {
     console.log('JQuery is not found');
 }
 else {
     reklamlariKaldir();
-    videoDuzenle();
+    removeOnlineUserBlock();
+    removeSponsor();
+    //videoDuzenle();
+    removeComments();
+    //removeFooter();
+    $('script[src*="ad.donanimhaber.com"]').remove();
+    $('script[src*="ad.e-kolay"]').remove();
+    $('param[value*="adserve.donanimhaber.com"]').parent().remove();
+}
+
+function removeFooter() {
+    // $('.info').remove();
+}
+
+function removeComments() {
+    $('.yorumlar').remove();
+}
+
+function removeSponsor() {
+    $('.forumSponsor').remove();
+    $('.sponsor').remove();
+}
+function removeOnlineUserBlock() {
+    $('center > table:contains(Şu andaki aktif kişilerin  )').remove();
 }
 
 function videoDuzenle() {
@@ -28,15 +52,49 @@ function videoDuzenle() {
     });
 }
 function reklamlariKaldir() {
-    var lst = $('script[src*="ad.donanimhaber.com"]').parent();
-    var lstID = ['dhmanset', 'paginate-dhmanset', 'ctl16_pnlVideoPlayer'];
-    var lstClassName = ['ochIcerik', 'haberSag'];
-    removeFromListById(lst, lstID);
-    removeFromListByClassName(lst, lstClassName);
-    console.log(lst);
-    lst.remove();
-    $('.yorumlar').remove();
-    $('.sponsor').remove();
+
+    $('.menuheader').remove();
+    $('.konulisteTextReklam').remove();
+    $('a[href*="donanimhaber.com/ttnet"]').remove();
+    if(window.location.href == 'http://forum.donanimhaber.com/') {
+        $('.msg').remove();
+        $($('a[href*="ad.donanimhaber.com"]').parent()).remove();
+        $($('table#Table3 >tbody>tr>td')[1]).remove();
+        $('table#Table3 >tbody>tr>td>table').css('width', '100%');
+    } else if(window.location.href.indexOf('http://forum.donanimhaber.com/forumid_') == 0) {
+        $($(Table5).parent()).remove();
+        $($('table')[3]).remove();
+        $($('table')[3]).remove();
+        $($('center table table')[7]).remove();
+        $($('.c2:contains("Reklam")').parent()).remove();
+
+    } else if(window.location.href.indexOf('http://forum.donanimhaber.com/m_') == 0) {
+        $($(Table2).parent()).remove();
+        $($('table[width*="95%"]')[1]).remove();
+        $($('table[width*="95%"]')[1]).remove();
+        $($('table[width*="95%"]')[$('table[width*="95%"]').length - 2]).remove();
+    } else if(window.location.href == 'http://donanimhaber.com/') {
+        var lst = $('script[src*="ad.donanimhaber.com"]').parent();
+        var lstID = ['dhmanset', 'paginate-dhmanset', 'ctl16_pnlVideoPlayer'];
+        var lstClassName = ['ochIcerik', 'haberSag'];
+        var lstTagName = ['body', 'center'];
+        removeFromListById(lst, lstID);
+        removeFromListByClassName(lst, lstClassName);
+        removeFromListByTagName(lst, lstTagName);
+        lst.remove();
+    } else if(window.location.href.indexOf('http://www.donanimhaber.com/') == 0) {
+        $('.divDikBannerAnasayfa').remove();
+        $(ctl16_divReklamUcuzcu).remove();
+        $(topbanner).remove();
+    }
+}
+
+function removeFromListByTagName(lst, lstTagName) {
+    $(lstTagName).each(function(i) {
+        if($.inArray(document.getElementsByTagName(lstTagName[i])[0], lst) != -1) {
+            lst.splice($.inArray(document.getElementsByTagName(lstTagName[i])[0], lst), 1);
+        }
+    });
 }
 
 function removeFromListById(lst, lstID) {
